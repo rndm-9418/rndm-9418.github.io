9f0739267c5c88a5ac6b4164466eccb851b15261
rndm-9418 <ev_galygin@yahoo.com>
2017-10-16 16:18:05 +0300

    Adding site

--- testing/immo/dev-royalcondo.org_files/js.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function(){
-		jQuery("#news .news_type").click(function(){
-			jQuery("#news .news_type").removeClass("massive-button_pushed");
-			jQuery(this).addClass("massive-button_pushed");
-			jQuery("#news .cards__item").hide();
-			jQuery("#news .type_"+jQuery(this).data('type')).css({'display': 'inline-block'});
-		});
-});
