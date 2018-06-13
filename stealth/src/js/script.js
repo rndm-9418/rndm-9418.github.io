ec018ff92408e88969aabcaf09d04ad0005b7702
rndm-9418 <ev_galygin@yahoo.com>
2018-06-13 12:07:57 +0400

    починил баг меню

--- stealth/src/js/script.js
+++ stealth/src/js/script.js
@@ -1,16 +1,13 @@
 function dropDown(){
-
+	
 	var toggle = ".js-dropdown-toggle";
 	var list = ".js-dropdown-list";
 
 	$(toggle).on("click", function(){
-
 		$(this).toggleClass("js-dropdown-toggle-unfolded");
-		$(list).toggle();
+		$(list).toggleClass("js-visible");
 
 	})
-
-	
 }
 
 dropDown();
