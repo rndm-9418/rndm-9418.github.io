db40fbb5ae6c0550b32da6f3158b6d7d3469b331
rndm-9418 <ev_galygin@yahoo.com>
2018-06-13 18:00:57 +0400

    добавил окошки

--- stealth/src/js/script.js
+++ stealth/src/js/script.js
@@ -1,5 +1,5 @@
 function dropDown(){
-	
+
 	var toggle = ".js-dropdown-toggle";
 	var list = ".js-dropdown-list";
 
@@ -11,3 +11,23 @@ function dropDown(){
 }
 
 dropDown();
+
+function popup(){
+	var popupIcon = ".js-popup-icon";
+	var popup = ".js-popup";
+
+	$(popupIcon).on("click", function(){
+		$(".js-popup-icon").not(this).each(function() {
+			$(this).find(popup).hide();
+			$(this).removeClass("js-z");
+		});
+
+		$(this).toggleClass("js-z");
+
+		$(this).find(popup).toggle();
+	})
+
+
+}
+
+popup();
